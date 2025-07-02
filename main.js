d2e4109f072ec749792bf744d8dbfd23871ba487
Francesco Nattino <49899980+fnattino@users.noreply.github.com>
2025-07-02 09:41:01 +0000

    add map visualization

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 import * as maplibregl from "https://esm.sh/maplibre-gl";
 
+import { Protocol } from "https://esm.sh/pmtiles";
+
+const protocol = new Protocol();
+
+maplibregl.addProtocol("pmtiles", protocol.tile);
+
 const map = new maplibregl.Map({
     container: 'mijnkaart', // container id
-    style: 'https://demotiles.maplibre.org/style.json', // style URL
-    center: [0, 0], // starting position [lng, lat]
-    zoom: 1 // starting zoom
+    style: './assets/style.json', // style URL
+    center: [9.35, 45.85], // starting position [lng, lat]
+    zoom: 13 // starting zoom
 });
